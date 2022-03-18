3ee19a2dbbfb57d064736a6297a3f702df811bc4
iamhi <inpactt@gmail.com>
2022-03-18 20:32:04 +0100

    Initial

---
+++ src/main/java/com/github/iamhi/hiintegration/confusedseeker/ConfusedSeekerApplication.java
@@ -0,0 +1,13 @@
+package com.github.iamhi.hiintegration.confusedseeker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfusedSeekerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfusedSeekerApplication.class, args);
+	}
+
+}
